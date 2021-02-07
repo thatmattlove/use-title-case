2a402c2be6cd6f2d63a07f2c808c6c71b6cf14ab
checktheroads <mtt.lov@gmail.com>
2021-02-07 00:24:33 -0700

    fix missing rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import babel from '@rollup/plugin-babel';
 import pkg from './package.json';
 
@@ -15,6 +16,7 @@ export default {
   external: [],
 
   plugins: [
+    typescript(),
     // Allows node_modules resolution
     resolve({ extensions }),
 
