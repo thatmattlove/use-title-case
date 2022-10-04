f661d4a3fa56f0421942ff769722797fc0daabcf
thatmattlove <mtt.lov@gmail.com>
2022-10-04 12:38:17 -0400

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,3 +1,4 @@
+import * as React from "react";
 import type { Config } from "jest";
 
 export default {
@@ -8,9 +9,15 @@ export default {
   testEnvironment: "jsdom",
   moduleDirectories: ["node_modules"],
   transform: {
-    "^.+\\.tsx?$": ["esbuild-jest", { sourcemap: "inline" }],
+    "^.+\\.tsx?$": [
+      "esbuild-jest",
+      { sourcemap: "inline", target: ["esnext"], platform: "browser", jsx: "preserve" },
+    ],
   },
   testPathIgnorePatterns: ["node_modules"],
   passWithNoTests: true,
   testMatch: ["<rootDir>/src/**/*.@(spec|test).ts", "<rootDir>/src/**/*.@(spec|test).tsx"],
+  globals: {
+    React,
+  },
 } as Config;
